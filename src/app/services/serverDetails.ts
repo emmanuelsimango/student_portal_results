fd7ccec1b9aa2cfaddad40ef5871cd80099b3f76
emmanuelsimango <emmanuelsimango@gmail.com>
2020-09-17 13:11:18 +0200

    Script

--- src/app/services/serverDetails.ts
+++ src/app/services/serverDetails.ts
@@ -5,8 +5,8 @@ export class ServerDetails {
 	public noticeAPI = `${this.websiteContentUrl}/api`;
 
 	public noticeImageStorage = `${this.noticeAPI}`;
-	// public serverIp = 'https://www.cut.ac.zw/portal';
-	public serverIp = 'http://172.16.50.109/portal';
+	public serverIp = 'https://www.cut.ac.zw/portal';
+	// public serverIp = 'http://172.16.50.109/portal';
 	// public serverIp = 'http://portal.local';
 	public portalURL = `${this.serverIp}`;
 	public loginURL = `${this.portalURL}/index.php/portal/login`;
