95e70d4a59a3ea7ae41aef7d9bc74c054dd4f0b3
gore chinoda <gorechinoda@gmail.com>
2020-09-20 08:56:26 +0200

    okay

--- src/app/services/serverDetails.ts
+++ src/app/services/serverDetails.ts
@@ -1,15 +1,15 @@
 export class ServerDetails {
-	// public serverDetails = "http://172.16.50.109/elearning.student.api/Procedures";
+	public serverDetails = "http://172.16.50.109/elearning.student.api/Procedures";
 	// public serverDetailsForApi = `${this.serverDetails}/api`;
 	// public serverDetailsForStorage = `${this.serverDetails}`;
 	// public portalURL = "http://portal.local";
 	// public studentServerDetails = "http://portal.local/index.php/cut_elearning";
-	public portalURL = "https://www.cut.ac.zw/portal";
+	public portalURL = "http://172.16.50.109/portal";
 	public loginURL = `${this.portalURL}/index.php/portal/login`;
 	public logoutURL = `${this.portalURL}/index.php/cut_elearning/home/logout`;
 	public vleLink = `${this.portalURL}/index.php/cut_elearning/api/loginToVitualLearning`;
-	public serverDetails = "https://cut.ac.zw/results/Procedures";
-	public studentServerDetails = "https://www.cut.ac.zw/portal/index.php/cut_elearning";
+	// public serverDetails = "https://cut.ac.zw/results/Procedures";
+	public studentServerDetails = "http://172.16.50.109/portal/index.php/cut_elearning";
 	public serverDetailsForApi = `${this.serverDetails}`;
 	public serverDetailsForStorage = `${this.serverDetails}`;
 }
