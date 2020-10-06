d7d424dfda4a12e6d0acb8065905db11669d9dd2
emmanuelsimango <emmanuelsimango@gmail.com>
2020-10-06 14:09:10 +0200

    Hie

--- src/app/models/registrationTemplate.ts
+++ src/app/models/registrationTemplate.ts
@@ -2,20 +2,25 @@ import { Student_period } from './student_period';
 
 export interface RegistrationTemplate {
 
-		"valid": boolean,
-		"message": String,
-		"body": {
-			"open": boolean,
-			"fees": number,
-			"module": [ {"module_code": string,
+	"valid": boolean,
+	"message": String,
+	"body": {
+		"open": boolean,
+		"fees": number,
+		"module": [{
+			"module_code": string,
 			"module_unit_code": number,
 			"module_id": number,
-			"module_name": string}],
-			"nextPeriod": Student_period,
-			"nextLevel": {
-				"semester": number,
-				"academic_level": number
-			}
+			"module_name": string,
+			"is_optional": boolean,
+            "has_selective": number,
+            "registration_choice_id": number
+		}],
+		"nextPeriod": Student_period,
+		"nextLevel": {
+			"semester": number,
+			"academic_level": number
 		}
+	}
 
 }
